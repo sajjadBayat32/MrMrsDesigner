5ddecfa68d2611b024a14d52333e8a5d089aaad7
sajjadBayat32 <sajjad@email.com>
2020-06-08 17:41:35 +0430

    section1, 2 and 3 completed with default images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import en from '@angular/common/locales/en';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './header/header.component';
 import {NgZorroModule} from './ng-zorro.module';
+import { HomeSection1Component } from './home-section1/home-section1.component';
+import { HomeSection2Component } from './home-section2/home-section2.component';
+import { HomeSection3Component } from './home-section3/home-section3.component';
 
 registerLocaleData(en);
 
@@ -18,7 +21,10 @@ registerLocaleData(en);
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeSection1Component,
+    HomeSection2Component,
+    HomeSection3Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home-section1/home-section1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-section1',
+  templateUrl: './home-section1.component.html',
+  styleUrls: ['./home-section1.component.css']
+})
+export class HomeSection1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
